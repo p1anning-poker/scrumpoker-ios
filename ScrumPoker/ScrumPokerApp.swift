a3c223a36f6aaacbd4c7a9d7205bb21b62b0db32
a.konshin <a.konshin@aximetria.com>
2022-09-15 11:40:34 +0300

    Initial Commit

---
+++ ScrumPoker/ScrumPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumPokerApp.swift
+//  ScrumPoker
+//
+//  Created by Aleksey Konshin on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
